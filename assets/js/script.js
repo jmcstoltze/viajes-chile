d3ebbdb028d56e223642fe9f3dd14ac6ec8117bc
Jose Contreras Stoltze <jmcontreras.stoltze@gmail.com>
2024-02-02 14:32:07 -0300

    Commit Quinto - Responsividad y ajustes finales

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
         {
           scrollTop: $(hash).offset().top,
         },
-        1500,
+        1200,
         function () {
           window.location.hash = hash;
         }
