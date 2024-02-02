583de2c1a5a874e7ed809b266d1f4764f1ea20d5
Jose Contreras Stoltze <jmcontreras.stoltze@gmail.com>
2024-02-02 13:23:47 -0300

    Commit Cuarto - Implementación de JavaScript y jQuery (carrusel, tooltips, smooth scroll y alert)

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// Smooth scroll
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        1500,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+// Tooltip de botones sección quiénes somos
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+// Botón enviar correo
+$("#enviar").click(function () {
+  alert("Sus datos fueron enviados correctamente...");
+});
